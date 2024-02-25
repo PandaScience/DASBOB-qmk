f9e5473de84d3143648b345690b961a0f974cd8a
René Wirnata <rene.wirnata@pandascience.net>
2024-02-25 11:48:14 +0100

    Enable/Disable (un)required features

--- rules.mk
+++ rules.mk
@@ -1,25 +1,47 @@
-
-
-# Bootloader selection
+# bootloader
 BOOTLOADER = rp2040
 
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-UNICODE_ENABLE = yes        # Unicode
-AUDIO_ENABLE = yes           # Audio output
-SPLIT_KEYBOARD = yes        # Use shared split_common code
-OLED_ENABLE = yes
-OLED_DRIVER = ssd1306
-WPM_ENABLE = yes
-LTO_ENABLE = yes
+# setup: https://docs.qmk.fm/#/serial_driver?id=setup-1
+SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = vendor
 AUDIO_DRIVER = pwm_hardware
+
+# OLED config
+OLED_ENABLE = no
+OLED_DRIVER = ssd1306
+
+# Link Time Optimization -> smaller size
+LTO_ENABLE = yes
+
+# disable stuff we don't need to further reduce size
+# https://docs.qmk.fm/#/squeezing_avr?id=rulesmk-settings
+CONSOLE_ENABLE = no         # console for debug
+COMMAND_ENABLE = no         # commands for debug and configuration
+BACKLIGHT_ENABLE = no       # keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # keyboard RGB underglow
+SPACE_CADET_ENABLE = no     # braces via shift tapping
+GRAVE_ESC_ENABLE = no       # shared ESC / grave key
+MAGIC_ENABLE = no           # swapping modifier keys etc.
+NKRO_ENABLE = no            # N-Key Rollover
+MUSIC_ENABLE = no           # translation from keys to notes
+AUDIO_ENABLE = no           # audio output
+WPM_ENABLE = no             # make words-per-minute available
+
+# enable special keys
+MOUSEKEY_ENABLE = yes       # click, scroll etc.
+EXTRAKEY_ENABLE = yes       # audio and system control
+UNICODE_ENABLE = yes        # unicode
+
+# simplify shifting characters (requires disabled COMMAND feature)
+COMMAND_ENABLE = no
+CAPS_WORD_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+
+# enable overriding shifted keys
+KEY_OVERRIDE_ENABLE = yes
+
+# enable swap hands feature
+# SWAP_HANDS_ENABLE = yes
+
+# enable key combos
+# COMBO_ENABLE = yes
